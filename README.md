103e177bb180b2cf25b0e8190c32cdfe30ebb040
佐々木圭介 <keisuke.sasaki@nadia.bz>
2022-09-22 12:49:55 +0900

    [add] first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## docker
+### 起動
+```
+docker-compose up
+```
+
+### 停止
+```
+docker-compose down
+```
---
+++ app/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="aaa">
+    </div>
+  );
+}
+
+export default App;
